d077e3ddf38fcd21cdaff39af715b6508071da86
jefersonrosa <jsrgodoy@gmail.com>
2024-04-02 07:27:15 -0300

    add imagens

--- decrypter.py
+++ decrypter.py
@@ -7,7 +7,7 @@ file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
+## chave para a descriptografia
 key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
@@ -15,7 +15,7 @@ decrypt_data = aes.decrypt(file_data)
 ## remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+## criar um novo arquivo descriptografado
 new_file = "teste.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
